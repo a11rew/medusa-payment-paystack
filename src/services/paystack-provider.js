1b477222a6db63a474aae758d561c68e64f2a103
femi <oluwafemi.akinyemi@datamellon.com>
2022-10-12 09:16:48 +0100

    Initiate paystack provider content

---
+++ src/services/paystack-provider.js
@@ -0,0 +1,16 @@
+import Stripe from "stripe";
+import {
+  AbstractPaymentService,
+  PaymentSessionData,
+  PaymentSessionStatus,
+} from "@medusajs/medusa";
+
+class PayStackProviderService extends AbstractPaymentService {
+  // static identifier = "paystack";
+
+
+  
+
+}
+
+export default PayStackProviderService;
