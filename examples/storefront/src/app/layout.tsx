a0f05cfbe243ccc9a06287efbed552e11d1776b4
Andrew Glago <andrewglago1@gmail.com>
2024-01-26 04:16:02 +0000

    chore: demo analytics

--- examples/storefront/src/app/layout.tsx
+++ examples/storefront/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import Providers from "@modules/providers"
+import Script from "next/script"
 import "styles/globals.css"
 
 export default function RootLayout({
@@ -8,6 +9,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-mode="light">
+      <Script
+        defer
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_ID}`}
+      />
+      <Script id="google-analytics">
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS_ID}');`}
+      </Script>
       <body>
         <Providers>
           <main className="relative">{children}</main>
